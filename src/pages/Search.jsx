51006e22e5fe5b3204c4bf3eb1674aa45ff7749a
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-13 19:57:20 -0300

    termina requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -6,6 +6,8 @@ import Header from '../components/Header';
 class Search extends Component {
   state = {
     loading: true,
+    artistName: '',
+    buttonDisable: true,
   };
 
   componentDidMount() {
@@ -21,12 +23,50 @@ class Search extends Component {
     }
   };
 
+  buttonVerify = () => {
+    const MIN_CHAR = 2;
+    const { artistName } = this.state;
+    if (artistName.length >= MIN_CHAR) {
+      return this.setState({
+        buttonDisable: false,
+      });
+    }
+    this.setState({
+      buttonDisable: true,
+    });
+  };
+
+  handleInputChange = ({ target }) => {
+    const { name, type, checked, value } = target;
+    const input = type === 'checkbox' ? checked : value;
+    this.setState({
+      [name]: input,
+    }, () => {
+      this.buttonVerify();
+    });
+  };
+
   render() {
-    const { loading } = this.state;
+    const { loading, artistName, buttonDisable } = this.state;
     return (
       <div data-testid="page-search">
         {loading && <Loading /> }
         <Header />
+        <input
+          type="text"
+          name="artistName"
+          id="artistName"
+          data-testid="search-artist-input"
+          value={ artistName }
+          onChange={ this.handleInputChange }
+        />
+        <input
+          type="button"
+          value="Pesquisar"
+          disabled={ buttonDisable }
+          data-testid="search-artist-button"
+          // onClick={ this.clickVerify }
+        />
       </div>
     );
   }

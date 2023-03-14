063afec381459a587ecb2a9170c9a7155b1d1756
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-13 22:41:38 -0300

    termina requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   state = {
     loading: true,
+    loadingArtist: false,
     artistName: '',
     buttonDisable: true,
+    showAlbums: false,
+    albums: [],
+    artistNameSaved: '',
   };
 
   componentDidMount() {
     this.loadingVerify();
+    // const { albums } = this.state;
+    // if (albums !== []) {
+    //   this.setState({
+    //     showAlbums: true,
+    //   });
+    // }
   }
 
   loadingVerify = async () => {
@@ -46,8 +58,43 @@ class Search extends Component {
     });
   };
 
+  albunsRes = async () => {
+    const { artistName } = this.state;
+    const albumResponse = await searchAlbumsAPI(artistName);
+    if (albumResponse !== []) {
+      this.setState({
+        albums: albumResponse,
+        showAlbums: true,
+        artistName: '',
+      });
+    }
+  };
+
+  clickButton = () => {
+    const { artistName } = this.state;
+    this.setState({
+      loadingArtist: true,
+      showAlbums: false,
+      artistNameSaved: artistName,
+    });
+    this.albunsRes();
+  };
+
   render() {
-    const { loading, artistName, buttonDisable } = this.state;
+    const { loading, artistName, buttonDisable, showAlbums, albums,
+      loadingArtist, artistNameSaved } = this.state;
+    const artist = albums.map((data, i) => (
+      <div key={ i }>
+        <Link
+          to={ `/album/${data.collectionId}` }
+          data-testid={ `link-to-album-${data.collectionId}` }
+        >
+          <img key={ i } src={ data.artworkUrl100 } alt="" />
+          <p>{data.collectionName}</p>
+          <p>{data.artistName}</p>
+        </Link>
+      </div>
+    ));
     return (
       <div data-testid="page-search">
         {loading && <Loading /> }
@@ -65,8 +112,25 @@ class Search extends Component {
           value="Pesquisar"
           disabled={ buttonDisable }
           data-testid="search-artist-button"
-          // onClick={ this.clickVerify }
+          onClick={ this.clickButton }
         />
+        {loadingArtist && (
+          !showAlbums ? <Loading />
+            : (
+              <div>
+                {albums.length === 0 ? <p>Nenhum álbum foi encontrado</p> : (
+                  <div>
+                    <p>{`Resultado de álbuns de: ${artistNameSaved}`}</p>
+                    {artist}
+                  </div>)}
+              </div>))}
+
+        {/* {!loadingArtist && showAlbums ? <Loading />
+          : (
+            <div>
+              <p>Resultado de álbuns de:</p>
+              {artist}
+            </div>)} */}
       </div>
     );
   }
